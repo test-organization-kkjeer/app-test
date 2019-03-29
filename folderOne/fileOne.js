f844065901fe1a196819cd7fc527fc49f8b7cd50
kkjeer <kkjeer@wellesley.edu>
2019-03-29 09:50:02 -0700

    Revert "Updated automerge test"

--- folderOne/fileOne.js
+++ folderOne/fileOne.js
@@ -4,3 +4,4 @@ console.log("file one");
 console.log("file one targeting branch master");
 console.log("addressing bot-tester's requests");
 console.log("test change for automerge");
+console.log("more changes");
